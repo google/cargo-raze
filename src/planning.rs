25c9eb0ef00956e08aac907cdd26ba3c90419e2d
Alex McArther <acmcarther@gmail.com>
2018-03-15 19:48:40 -0700

    Cleanup

--- src/planning.rs
+++ src/planning.rs
@@ -364,9 +364,6 @@ impl<'fetcher> BuildPlannerImpl<'fetcher> {
   }
 
   fn produce_targets(&self, package: &Package) -> CargoResult<Vec<BuildTarget>> {
-    let full_name = format!("{}-{}", package.name, package.version);
-    let partial_path = format!("{}/", full_name);
-    let partial_path_byte_length = partial_path.as_bytes().len();
     let mut targets = Vec::new();
     for target in package.targets.iter() {
       let manifest_pathbuf = PathBuf::from(&package.manifest_path);
@@ -444,14 +441,10 @@ fn load_and_dedup_licenses(licenses: &str) -> Vec<LicenseData> {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use hamcrest::core::expect;
-  use hamcrest::prelude::*;
   use metadata::Metadata;
-  use metadata::MetadataFetcher;
   use metadata::ResolveNode;
   use metadata::testing as metadata_testing;
   use metadata::testing::StubMetadataFetcher;
-  use settings::RazeSettings;
   use settings::testing as settings_testing;
 
   // Indexes into hand-crafted metadata `resolve.nodes` and `packages`
