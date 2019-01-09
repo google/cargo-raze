8a75aa024a70a60fbf3c4a847078df495061a5f0
Kornelijus Survila <kornholijo@gmail.com>
2019-01-09 16:05:39 -0700

    Bump Bazel on Travis to 0.17.1 (#81)
    * Bump Bazel on Travis to 0.17.1
    
    * Update rules_rust in CI
    
    * Add bazel_version to the WORKSPACE

--- smoke-test.sh
+++ smoke-test.sh
@@ -29,9 +29,18 @@ workspace(name = "io_bazel_rules_rust")
 
 git_repository(
     name = "io_bazel_rules_rust",
-    commit = "af9821bf3378b525ec3db0af3b1ca388920a8fb0",
+    commit = "f32695dcd02d9a19e42b9eb7f29a24a8ceb2b858",
     remote = "https://github.com/bazelbuild/rules_rust.git",
 )
+
+http_archive(
+    name = "bazel_skylib",
+    sha256 = "eb5c57e4c12e68c0c20bc774bfbc60a568e800d025557bc4ea022c6479acc867",
+    strip_prefix = "bazel-skylib-0.6.0",
+    url = "https://github.com/bazelbuild/bazel-skylib/archive/0.6.0.tar.gz",
+)
+load("@io_bazel_rules_rust//:workspace.bzl", "bazel_version")
+bazel_version(name = "bazel_version")
 
 load("@io_bazel_rules_rust//rust:repositories.bzl", "rust_repositories")
 rust_repositories()
