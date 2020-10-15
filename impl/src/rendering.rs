43468c0b55174952bf6796c99652be86bc2a69e7
Andre Brisco <andre.brisco@gmail.com>
2020-10-14 21:36:19 -0700

    Turned the `rendering` module into a directory that contains all relevant components

--- impl/src/rendering.rs
+++ impl/src/rendering.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod bazel;
+
 use crate::planning::PlannedBuild;
 use std::path::PathBuf;
 use anyhow::Result;
