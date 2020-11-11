4a8c7a1321b41e0400d54e3ebeb8ec5e86aa78b4
Piotr Sikora <piotrsikora@google.com>
2020-11-11 01:00:47 +0000

    Add support for aarch64-apple-darwin target.
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- smoke-test.sh
+++ smoke-test.sh
@@ -40,11 +40,11 @@ load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 http_archive(
     name = "io_bazel_rules_rust",
-    sha256 = "17dbf791f4dab0fd4496ce5345af35e9ce2f6d011c1c8423436da517d019a3ea",
-    strip_prefix = "rules_rust-2f97db595b05b1ee8cc44bde5bdf03c00bd169fb",
+    sha256 = "b5d4d1c7609714dfef821355f40353c58aa1afb3803401b3442ed2355db9b0c7",
+    strip_prefix = "rules_rust-8d2b4eeeff9dce24f5cbb36018f2d60ecd676639",
     urls = [
-        # Master branch as of 2020-11-05
-        "https://github.com/bazelbuild/rules_rust/archive/2f97db595b05b1ee8cc44bde5bdf03c00bd169fb.tar.gz",
+        # Master branch as of 2020-11-10
+        "https://github.com/bazelbuild/rules_rust/archive/8d2b4eeeff9dce24f5cbb36018f2d60ecd676639.tar.gz",
     ],
 )
 
